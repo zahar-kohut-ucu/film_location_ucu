d53cf5ac9b852d01ab3391b80e1630fc1a709e89
Zahar Kohut <zaharentv2005@gmail.com>
2023-02-21 19:31:05 +0200

    First full soft version, database changes

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import argparse
 import folium
 import pandas as pd
+from geopy.exc import GeocoderUnavailable
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic as GD
 
 parser = argparse.ArgumentParser()
 
@@ -24,12 +27,29 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-def main():
+def main(year, latitude, longitude, path):
     '''Main function.'''
-    a = args.year
-    map = folium.Map(tiles="Stamen Terrain")
+    map = folium.Map(location=[52.479618,62.185832], zoom_start=10)
+    movies_layer = folium.FeatureGroup(name='Movies')
+    data = pd.read_csv(path, sep=';')
+    data = data.reset_index()
+    geolocator = Nominatim(user_agent="Kohut_Map")
+    locations = []
+    for index, row in data.iterrows():
+        if row.year == int(year):
+            location = geolocator.geocode(row.address)
+            if location:
+                if GD((location.latitude, location.longitude), (float(latitude), float(longitude))).km < 3000:
+                    locations.append((row.movie, (location.latitude, location.longitude)))
+            if len(locations) > 6:
+                break
+    for name, coords in locations:
+        movies_layer.add_child(folium.Marker(location=[coords[0], coords[1]],
+                                             popup=name,
+                                             icon=folium.Icon()))
+    map.add_child(movies_layer)
     map.save('test.html')
-
+    return None
 
 if __name__ == '__main__':
-    main()
+    main(args.year, args.latitude, args.latitude, args.path)
