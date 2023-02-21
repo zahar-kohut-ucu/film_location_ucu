c3cdfb2602e6ee424ec6edc1906e0ae3b0dee6cb
Zahar Kohut <zaharentv2005@gmail.com>
2023-02-21 17:35:51 +0200

    Added parser

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+'''Lab #1.2'''
+import argparse
+import folium
+import pandas as pd
 
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    'year',
+    help = 'Year of film'
+)
+parser.add_argument(
+    'latitude',
+    help = 'Your latitude'
+)
+parser.add_argument(
+    'longitude',
+    help = 'Your longitude'
+)
+parser.add_argument(
+    'path',
+    help = 'Path to database'
+)
+
+args = parser.parse_args()
+
+def main():
+    '''Main function.'''
+    a = args.year
+    map = folium.Map(tiles="Stamen Terrain")
+    map.save('test.html')
+
+
+if __name__ == '__main__':
+    main()
