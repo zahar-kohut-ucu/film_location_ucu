6efa82c68231681f0bf63fd3e7f6e66bbaefa1ce
Zahar Kohut <zaharentv2005@gmail.com>
2023-02-21 20:37:34 +0200

    First and second layer ready

--- main.py
+++ main.py
@@ -29,27 +29,38 @@ args = parser.parse_args()
 
 def main(year, latitude, longitude, path):
     '''Main function.'''
-    map = folium.Map(location=[52.479618,62.185832], zoom_start=10)
+    map = folium.Map(location=[float(latitude), float(longitude)], zoom_start=5)
+    default_layer = folium.FeatureGroup(name='Default')
     movies_layer = folium.FeatureGroup(name='Movies')
     data = pd.read_csv(path, sep=';')
     data = data.reset_index()
     geolocator = Nominatim(user_agent="Kohut_Map")
-    locations = []
+    locations = set()
     for index, row in data.iterrows():
         if row.year == int(year):
-            location = geolocator.geocode(row.address)
+            try:
+                location = geolocator.geocode(row.address)
+            except GeocoderUnavailable:
+                continue
             if location:
-                if GD((location.latitude, location.longitude), (float(latitude), float(longitude))).km < 3000:
-                    locations.append((row.movie, (location.latitude, location.longitude)))
-            if len(locations) > 6:
+                if GD((location.latitude, location.longitude), \
+                (float(latitude), float(longitude))).km < 3000:
+                    locations.add((row.movie, (location.latitude, location.longitude)))
+            if len(locations) > 9:
                 break
     for name, coords in locations:
         movies_layer.add_child(folium.Marker(location=[coords[0], coords[1]],
                                              popup=name,
                                              icon=folium.Icon()))
-    map.add_child(movies_layer)
+    default_layer.add_child(folium.Marker(location=[float(latitude), float(longitude)],
+                                          popup='Your location.',
+                                          icon=folium.Icon(color = "red")))
+    default_layer.add_to(map)
+    movies_layer.add_to(map)
+    folium.LayerControl(collapsed=False).add_to(map)
     map.save('test.html')
-    return None
+    if not locations:
+        print('No movies which suit your data.')
 
 if __name__ == '__main__':
-    main(args.year, args.latitude, args.latitude, args.path)
+    main(args.year, args.latitude, args.longitude, args.path)
